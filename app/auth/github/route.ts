43cd6eb8c04c630c47c871e9b226877305589eac
Juan Alvarez <juan@nemphi.com>
2024-09-02 16:41:21 -0500

    fix: try to fix login redirect again

--- app/auth/github/route.ts
+++ app/auth/github/route.ts
@@ -1,12 +1,8 @@
 import { API_BASE_URL } from "@/lib/api"
-import { cookies } from "next/headers"
+import { NextResponse } from "next/server"
 
 export const runtime = "edge"
 
-export function GET() {
-	return fetch(`${API_BASE_URL}/auth/github`, {
-		headers: {
-			Cookie: cookies().toString()
-		}
-	})
+export async function GET() {
+	return NextResponse.redirect(new URL(`${API_BASE_URL}/auth/github`))
 }
