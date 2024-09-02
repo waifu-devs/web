3482f4faf90ec0e11015bd6506f87b866702f070
Juan Alvarez <juan@nemphi.com>
2024-09-02 17:35:30 -0500

    chore: change login btn url

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,26 +16,26 @@ export default function Header() {
   return (
     <header className='bg-white sticky top-0 z-[20] w-[80%] self-center'>
       <div className='flex flex-row justify-between w-full px-4 py-2'>
-      <div className='flex flex-row items-center'>
-        <Logo/>
-        <h1 className='text-xl dark:text-whitetext-2xl md:text-xl lg:text-4xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug mx-auto'>Waifu.dev</h1>
-      </div>
-      <NavigationMenu>
-        <NavigationMenuList>
-        <NavigationMenuItem>
-          <Link href="/docs" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              About
-            </NavigationMenuLink>
-          </Link>
-          <Link href="/auth/github" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyleShimmer()}>
-              Login
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+        <div className='flex flex-row items-center'>
+          <Logo />
+          <h1 className='text-xl dark:text-whitetext-2xl md:text-xl lg:text-4xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug mx-auto'>Waifu.dev</h1>
+        </div>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Link href="/docs" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  About
+                </NavigationMenuLink>
+              </Link>
+              <Link href="/auth" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyleShimmer()}>
+                  Login
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
       </div>
       <Separator className='h-[1px] bg-border' />
     </header>
