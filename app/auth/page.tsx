8a3d82f285696c2c7c4b395252a494ec12879628
Juan Alvarez <juan@nemphi.com>
2024-09-12 06:32:17 -0500

    chore: add link to go back home

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,6 @@
 import { API_BASE_URL, fetcher } from "@/lib/api"
 import { cookies } from "next/headers"
+import Link from "next/link"
 import { redirect } from "next/navigation"
 
 export const runtime = "edge"
@@ -15,6 +16,7 @@ export default async function Auth() {
   }
   const data = await res.json()
   return (<>
+    <Link href="/">Go back to home</Link>
     <h1>Auth</h1>
     <p>user id: {data.user.id}</p>
     <p>github id: {data.user.githubId}</p>
